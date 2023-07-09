f54c19c94377eaee655b04fbd41ef2326eaac3ae
Nithinvarma17 <107095446+Nithinvarma17@users.noreply.github.com>
2023-07-09 17:04:35 +0530

    Add files via upload

---
+++ lec9.java
@@ -0,0 +1,18 @@
+class father {
+ public void foodhabit(){
+    final int last=100;
+        System.out.println("hello i am hte father");
+    }
+}
+class sonn extends father{
+    //cannot change last as it is final;
+    //last=33;
+    public void foodhabit(){
+        System.out.println("hello i am hte son");
+    }
+}
+public class lec9 {
+    public static void main(String[] args) {
+        
+    }
+}
